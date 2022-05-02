48a379721a94bcf5e0d45afd79467e2fa9501d77
Kalif Purce <kalifpurce@gmail.com>
2022-05-01 17:06:43 -0700

    Begennings

---
+++ server.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+const sql = require('./sql');
+
+function begin() {
+console.log("Hello welcome to the main menu");
+mainmenu();
+}
+
+
+function  mainmenu() {
+inquirer.prompt([
+
+
+])
+}
