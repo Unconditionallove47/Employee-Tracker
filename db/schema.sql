6340d451ce32e387a2845fc783610786e44467e9
Kalif P <kalifpurce@gmail.com>
2022-05-31 17:36:10 -0700

    fixed tutor stuff

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE roles (
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
-
+/* seems to show red, but as i worked with my tutor it still functions properly*/
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
@@ -26,14 +26,3 @@ CREATE TABLE employee (
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
--- CREATE TABLE employee (
---   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
---   first_name VARCHAR(30) NOT NULL,
---   last_name VARCHAR(30) NOT NULL,
---   role_id INT UNSIGNED NOT NULL,
---   INDEX role_ind (role_id),
---   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
---   manager_id INT UNSIGNED,
---   INDEX man_ind (manager_id),
---   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
--- );
